f422fe99a521ee934e3a1841058a6ff617c68fbb
Альберт <albiqus@bk.ru>
2022-08-30 17:04:55 +0300

    изменил дизайн декорации

--- src/components/Decoration/Decoration.jsx
+++ src/components/Decoration/Decoration.jsx
@@ -4,7 +4,7 @@ import classes from './Decoration.module.css';
 const Decoration = (props) => {
     return (
         <div className={classes.decorationBox} >
-            <p >JUST DO IT</p>
+         
         </div>
     )
 }
